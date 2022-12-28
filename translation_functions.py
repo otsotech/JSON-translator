233cd63fae61aa7b97d9aa62594b14d79358eccb
unknown <otso.niemi@gmail.com>
2022-12-28 20:08:54 +0200

    Major code improvements.
    Fixed string skipping. Added comments.

--- translation_functions.py
+++ translation_functions.py
@@ -1,4 +1,5 @@
 import requests
+import re
 
 def translate(text, target_language, email):
     # MyMemory Translation API endpoint
@@ -19,38 +20,58 @@ def translate(text, target_language, email):
     return translated_text
 
 def translate_json(json_data, target_language, email, keys_processed=0, total_keys=None):
+    # Initialize total_keys to the number of keys in json_data if it is not provided
     if total_keys is None:
         total_keys = count_keys(json_data)
 
+    # If json_data is a dictionary, process each key-value pair
     if isinstance(json_data, dict):
         for key, value in json_data.items():
+            # Skip values that are not strings or have only one character
             if isinstance(value, str) and len(value) > 1:
-                if not value.strip() or len(value.strip()) == 1 or value.isdigit():
-                    print(f"Skipping '{value}' because it contains only a single letter or symbol or only numbers.")
+                # Skip values that do not contain a word (i.e., a sequence of one or more letters surrounded by word boundaries)
+                if not re.search(r'\b[a-zA-Z]+\b', value):
+                    print("Skipping '{}' because it does not contain a word.".format(value))
+                # Skip values that contain only a single letter
+                elif len(value.strip()) == 1:
+                    print("Skipping '{}' because it contains only a single letter.".format(value))
+                # Translate values that meet the above conditions
                 else:
-                    print(f"Translating '{value}'")
+                    print("Translating '{}'".format(value))
                     json_data[key] = translate(value, target_language, email)
+            # Recursively process values that are not strings or have only one character
             else:
                 translate_json(value, target_language, email, keys_processed, total_keys)
+            # Increment the keys_processed counter and print the percentage complete
             keys_processed += 1
             percentage_complete = keys_processed / total_keys * 100
-            print(f"{percentage_complete:.2f}% complete\n")
+            print("    {:.2f}% complete\n".format(percentage_complete))
+    # If json_data is a list, process each item
     elif isinstance(json_data, list):
         for i, item in enumerate(json_data):
+            # Skip items that are not strings or have only one character
             if isinstance(item, str) and len(item) > 1:
-                if not item.strip() or len(item.strip()) == 1 or item.isdigit():
-                    print(f"Skipping '{item}' because it contains only a single letter or symbol or only numbers.")
+                # Skip items that do not contain a word (i.e., a sequence of one or more letters surrounded by word boundaries)
+                if not re.search(r'\b[a-zA-Z]+\b', item):
+                    print("Skipping '{}' because it does not contain a word.".format(item))
+                # Skip items that contain only a single letter
+                elif len(item.strip()) == 1:
+                    print("Skipping '{}' because it contains only a single letter.".format(item))
+                # Translate items that meet the above conditions
                 else:
-                    print(f"Translating '{item}'")
+                    print("Translating '{}'\n".format(item))
                     json_data[i] = translate(item, target_language, email)
+            # Recursively process items that are not strings or have
             else:
                 translate_json(item, target_language, email, keys_processed, total_keys)
+            # Increment the keys_processed counter and print the percentage complete
             keys_processed += 1
             percentage_complete = keys_processed / total_keys * 100
-            print(f"{percentage_complete:.2f}% complete\n")
-    else:
-        print(f"Skipping '{json_data}' because it is not a string, dict, or list.")
+            print("    {:.2f}% complete".format(percentage_complete))
+    # Return the modified json_data
     return json_data
+
+
 
 def count_keys(data):
     total_keys = 0
