94c572561556484a0143d18c6eadce75b9321250
unknown <otso.niemi@gmail.com>
2022-12-28 23:39:03 +0200

    error handling.
    added the ability to handle errors. removed messy comments.

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
 import argparse
 import json
 
-from translation_functions import translate, translate_json
+from translation_functions import translate, translate_json, TranslationError
 
-if __name__ == "__main__":
-    # Parse command line arguments
+def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", required=True, help="input JSON file")
     parser.add_argument("-l", "--language", required=True, help="target language")
     parser.add_argument("-e", "--email", required=True, help="email address")
-    args = parser.parse_args()
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
     print(f"Translating file {args.input} to language {args.language}. Email: {args.email}\n")
 
-    # Load the JSON file
-    with open(args.input, "r", encoding="utf-8") as f:
-        json_data = json.load(f)
+    try:
+        with open(args.input, "r", encoding="utf-8") as f:
+            json_data = json.load(f)
+    except OSError as e:
+        print("An error occurred while reading the input file: {}".format(e))
+        exit(1)
+    except json.JSONDecodeError as e:
+        print("An error occurred while parsing the input file: {}".format(e))
+        exit(1)
 
-    # Translate the JSON data
-    translate_json(json_data, args.language, args.email)
+    try:
+        translate_json(json_data, args.language, args.email)
+    except TranslationError as e:
+        print("An error occurred while translating the JSON data: {}".format(e))
+        exit(1)
 
-    # Save the translated JSON data
-    with open(args.input, "w", encoding="utf-8") as f:
-        json.dump(json_data, f, indent=2)
+    try:
+        with open(args.input, "w", encoding="utf-8") as f:
+            json.dump(json_data, f, indent=2)
+    except OSError as e:
+        print("An error occurred while saving the translated JSON data to the file: {}".format(e))
+        exit(1)
+
     print("Saved translated JSON data to file.")
