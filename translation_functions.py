d6a23b628c559ba00e424f015e9346fef6180755
unknown <otso.niemi@gmail.com>
2022-12-28 23:39:26 +0200

    error handling.
    added the ability to handle errors. removed messy comments.

--- translation_functions.py
+++ translation_functions.py
@@ -2,78 +2,78 @@ import requests
 import re
 
 def translate(text, target_language, email):
-    # MyMemory Translation API endpoint
     endpoint = "http://api.mymemory.translated.net/get"
 
-    # Make a request to the API
     params = {
         "q": text,
         "langpair": f"en|{target_language}",
         "de": email,
     }
-    response = requests.get(endpoint, params=params)
+    try:
+        response = requests.get(endpoint, params=params)
+    except requests.RequestException as e:
+        raise TranslationError("An error occurred while making the request: {}".format(e))
 
-    # Parse the response
-    data = response.json()
-    translated_text = data["responseData"]["translatedText"]
+    try:
+        data = response.json()
+    except ValueError as e:
+        raise TranslationError("An error occurred while parsing the response: {}".format(e))
+
+    try:
+        translated_text = data["responseData"]["translatedText"]
+    except KeyError as e:
+        raise TranslationError("An error occurred while accessing the response data: {}".format(e))
 
     return translated_text
 
 def translate_json(json_data, target_language, email, keys_processed=0, total_keys=None):
-    # Initialize total_keys to the number of keys in json_data if it is not provided
     if total_keys is None:
-        total_keys = count_keys(json_data)
+        try:
+            total_keys = count_keys(json_data)
+        except ValueError as e:
+            raise TranslationError("An error occurred while counting the keys in the input data: {}".format(e))
 
-    # If json_data is a dictionary, process each key-value pair
     if isinstance(json_data, dict):
         for key, value in json_data.items():
-            # Skip values that are not strings or have only one character
             if isinstance(value, str) and len(value) > 1:
-                # Skip values that do not contain a word (i.e., a sequence of one or more letters surrounded by word boundaries)
                 if not re.search(r'\b[a-zA-Z]+\b', value):
                     print("Skipping '{}' because it does not contain a word.".format(value))
-                # Skip values that contain only a single letter
                 elif len(value.strip()) == 1:
                     print("Skipping '{}' because it contains only a single letter.".format(value))
-                # Translate values that meet the above conditions
                 else:
                     print("Translating '{}'".format(value))
-                    json_data[key] = translate(value, target_language, email)
-            # Recursively process values that are not strings or have only one character
+                    try:
+                        json_data[key] = translate(value, target_language, email)
+                    except TranslationError as e:
+                        print("Error occurred while translating '{}': {}".format(value, e))
             else:
                 translate_json(value, target_language, email, keys_processed, total_keys)
-            # Increment the keys_processed counter and print the percentage complete
             keys_processed += 1
             percentage_complete = keys_processed / total_keys * 100
             print("    {:.2f}% complete\n".format(percentage_complete))
-    # If json_data is a list, process each item
     elif isinstance(json_data, list):
         for i, item in enumerate(json_data):
-            # Skip items that are not strings or have only one character
             if isinstance(item, str) and len(item) > 1:
-                # Skip items that do not contain a word (i.e., a sequence of one or more letters surrounded by word boundaries)
                 if not re.search(r'\b[a-zA-Z]+\b', item):
                     print("Skipping '{}' because it does not contain a word.".format(item))
-                # Skip items that contain only a single letter
                 elif len(item.strip()) == 1:
                     print("Skipping '{}' because it contains only a single letter.".format(item))
-                # Translate items that meet the above conditions
                 else:
                     print("Translating '{}'\n".format(item))
-                    json_data[i] = translate(item, target_language, email)
-            # Recursively process items that are not strings or have
+                    try:
+                        json_data[i] = translate(item, target_language, email)
+                    except TranslationError as e:
+                        print("Error occurred while translating '{}': {}".format(item, e))
             else:
                 translate_json(item, target_language, email, keys_processed, total_keys)
-            # Increment the keys_processed counter and print the percentage complete
             keys_processed += 1
             percentage_complete = keys_processed / total_keys * 100
             print("    {:.2f}% complete".format(percentage_complete))
-    # Return the modified json_data
     return json_data
 
-
-
 def count_keys(data):
+    if not isinstance(data, (dict, list)):
+        raise ValueError("Input data must be a dictionary or a list")
     total_keys = 0
     stack = [data]
     while stack:
@@ -95,3 +95,6 @@ def traverse(data):
         for item in data:
             yield item
             yield from traverse(item)
+
+class TranslationError(Exception):
+    pass
