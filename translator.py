00f923de0de4c32d2616442e2b87f29935c174da
unknown <otso.niemi@gmail.com>
2022-12-28 02:22:25 +0200

    Progress bar. Code improvements.

--- translator.py
+++ translator.py
@@ -11,7 +11,6 @@ def translate(text, target_language):
         "q": text,
         "langpair": f"en|{target_language}",
     }
-    print(f"Making request to {endpoint} with params {params}")
     response = requests.get(endpoint, params=params)
 
     # Parse the response
@@ -20,26 +19,43 @@ def translate(text, target_language):
 
     return translated_text
 
-def translate_json(json_data, target_language):
+def translate_json(json_data, target_language, keys_processed=0, total_keys=None):
+    if total_keys is None:
+        # Calculate total number of keys in the JSON data
+        total_keys = 0
+        stack = [json_data]
+        while stack:
+            value = stack.pop()
+            if isinstance(value, dict):
+                total_keys += len(value)
+                stack.extend(value.values())
+            elif isinstance(value, list):
+                total_keys += len(value)
+                stack.extend(value)
+
     if isinstance(json_data, dict):
         for key, value in json_data.items():
-            print(f"Processing key {key} with value {value}")
             if isinstance(value, str) and len(value) > 1:
                 # Check if the string contains only a symbol
                 if not value.strip():
-                    print(f"Skipping string {value} because it contains only a symbol")
-                    continue
-                print(f"Translating string {value}")
-                json_data[key] = translate(value, target_language)
+                    print(f"Skipping '{value}' because it contains only a symbol.")
+                else:
+                    print(f"Translating '{value}'")
+                    json_data[key] = translate(value, target_language)
             else:
-                translate_json(value, target_language)
+                translate_json(value, target_language, keys_processed, total_keys)
+            keys_processed += 1
+            percentage_complete = keys_processed / total_keys * 100
+            print(f"{percentage_complete:.2f}% complete\n")
     elif isinstance(json_data, list):
         for item in json_data:
-            print(f"Processing item {item}")
-            translate_json(item, target_language)
+            translate_json(item, target_language, keys_processed, total_keys)
+            keys_processed += 1
+            percentage_complete = keys_processed / total_keys * 100
+            print(f"{percentage_complete:.2f}% complete\n")
     else:
         # Return immediately if the value is not a string, dict, or list
-        print(f"Skipping value {json_data} because it is not a string, dict, or list")
+        print(f"Skipping '{json_data}' because it is not a string, dict, or list.")
         return
 
 if __name__ == "__main__":
@@ -48,18 +64,16 @@ if __name__ == "__main__":
     parser.add_argument("-i", "--input", required=True, help="input JSON file")
     parser.add_argument("-l", "--language", required=True, help="target language")
     args = parser.parse_args()
-    print(f"Translating file {args.input} to language {args.language}")
+    print(f"Translating file {args.input} to language {args.language} \n")
 
     # Load the JSON file
     with open(args.input, "r") as f:
         json_data = json.load(f)
-    print(f"Loaded JSON data: {json_data}")
 
     # Translate the JSON data
     translate_json(json_data, args.language)
-    print(f"Translated JSON data: {json_data}")
 
     # Save the translated JSON data
     with open(args.input, "w") as f:
         json.dump(json_data, f, indent=2)
-    print("Saved translated JSON data to file")
+    print("Saved translated JSON data to file.")
